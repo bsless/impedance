9f82a59395a5405d9b78812e1ecebb76906b49e6
ben.sless <ben.sless@appsflyer.com>
2020-12-08 19:41:34 +0200

    Prepare project.clj for circleci

--- project.clj
+++ project.clj
@@ -1,23 +1,15 @@
-(defproject bsless/impedance "0.0.0-alpha1"
+(defproject bsless/impedance "0.0.0-alpha1-SNAPSHOT"
   :description "Fast Clojure map transformers and context"
   :url "https://github.com/bsless/impedance"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]]
   :deploy-repositories [["clojars" {:url "https://clojars.org/repo"
-                                    :username :gpg
-                                    :password :gpg
+                                    :username :env/clojars_user
+                                    :password :env/clojars_token
                                     :sign-releases false}]
                         ["releases" :clojars]
                         ["snapshots" :clojars]]
-  :release-tasks
-  [["vcs" "assert-committed"]
-   ["change" "version" "leiningen.release/bump-version" "release"]
-   ["vcs" "commit"]
-   ["deploy" "clojars"]
-   ["change" "version" "leiningen.release/bump-version"]
-   ["vcs" "commit"]
-   ["vcs" "push"]]
   :profiles
   {:dev
    {:source-paths ["bench"]
